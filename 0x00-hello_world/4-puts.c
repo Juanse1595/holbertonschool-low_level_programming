248a34da58cd3020360ba6ae55f2cbd2d7905394
Juanse1595 <3309@holbertonschool.com>
2021-06-18 01:43:27 +0000

    Fixing 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *main - Prints a specific text
- *
- *Description: This function prints the text 
- *that was given in the excercise
- *Return: zero to confirm everything went well
+ * main - Prints a specific text
+ * Description: This function prints the text
+ * that was given in the excercise
+ * Return: zero to confirm everything went well
  */
 int main(void)
 {
