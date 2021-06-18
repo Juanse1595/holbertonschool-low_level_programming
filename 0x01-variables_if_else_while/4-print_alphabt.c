2b5f389af81f926d254564a08c829fc35f6bfa59
Juanse1595 <3309@holbertonschool.com>
2021-06-18 23:24:35 +0000

    Adding 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet, not including e or q
+ * Description: Prints the alphabet wihtout printing e or q
+ * Return: 0 and the letters that were asked
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 123)
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
