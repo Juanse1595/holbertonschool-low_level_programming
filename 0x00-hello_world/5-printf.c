710118eb41faedc8c44cdf01b5a50ac37002e6c5
Juanse1595 <3309@holbertonschool.com>
2021-06-18 01:48:47 +0000

    Adding c program printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a text
+ * Description: This function prints another text
+ * Return: zero, meaning everything went well.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
