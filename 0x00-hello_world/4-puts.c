5e04901b77c404c5de19bbdf954e75b5f6026486
Juanse1595 <3309@holbertonschool.com>
2021-06-18 00:54:11 +0000

    Fixing puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-	char *a = "\"Programming is like building a multilingual puzzle\n";
+	char *a = "\"Programming is like building a multilingual puzzle";
 	puts(a);
 	return 0;
 }
