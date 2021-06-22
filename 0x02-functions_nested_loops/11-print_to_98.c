ccdf87e1421ba74a7d29bd05e3a677ed820655f2
Juanse1595 <3309@holbertonschool.com>
2021-06-22 23:53:41 +0000

    Adding 11th C program

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all the numbers between the given
+ * number and 98
+ * @n: given number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n", n);
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", n);
+	}
+}
