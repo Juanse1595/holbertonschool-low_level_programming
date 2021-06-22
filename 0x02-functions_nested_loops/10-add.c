a4abfdc80d5827b1010d7881c3c8f631fe4ba9d5
Juanse1595 <3309@holbertonschool.com>
2021-06-22 23:39:16 +0000

    Adding 10th C program add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the addition of both numbers
+ */
+
+int add (int a, int b)
+{
+	return (a + b);
+}
