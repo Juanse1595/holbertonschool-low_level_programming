07128ed8a1cb0cfcfa2b3914ff10405892623aec
Juanse1595 <3309@holbertonschool.com>
2021-06-22 15:48:16 +0000

    Fixing 4rd betty requirements

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * _isalpha - 
- * Description: 
- * Return:
+ * _isalpha - tells if it is a letter
+ * @c: given char
+ * Description: tells if the given char is a letter or anything else
+ * Return: 1 if letter, 0 otherwise
  */
 
 int _isalpha(int c)
