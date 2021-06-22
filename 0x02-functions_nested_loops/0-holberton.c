efb7d494aba75c4571ebbe393988af7ec3e13d6b
Juanse1595 <3309@holbertonschool.com>
2021-06-22 04:54:13 +0000

    Adding 0-holberton.c program

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - Prints "Holberton"
+ * Description: Prints "Hollberton" using the function _putchar
+ * that is included in the holberton.h header file
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_putchar(72);
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
