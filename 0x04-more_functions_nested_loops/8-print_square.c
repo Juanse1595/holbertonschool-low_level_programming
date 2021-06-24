64be5b8908fdad0b52f6e8fda391d352505f0257
Juanse1595 <3309@holbertonschool.com>
2021-06-24 20:44:47 +0000

    Adding 8th program

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a square made of #
+ * @size: the size of the square
+ * Return: void
+ */
+
+void print_square(int size);
+{
+	int i = 0, j = 0;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+			j = 0;
+		}
+	}
+	else
+		_putchar('\n');
+}
