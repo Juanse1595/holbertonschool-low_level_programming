615aae77cb76bbbbb0ee485dcbd5c1239d3fc1e6
Juanse1595 <3309@holbertonschool.com>
2021-06-24 17:55:03 +0000

    Adding 5th function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 skipping 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+		i++
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers form 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j = 0;
+
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			if (j > 9)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
