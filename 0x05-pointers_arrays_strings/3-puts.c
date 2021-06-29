470ec2af1e6e6656034db50c0a3eb069fb7f98b2
Juanse1595 <3309@holbertonschool.com>
2021-06-29 01:59:47 +0000

    Adding 3rd program

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string
+ * @str: adress of the first char of a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		_putchar(str[i]);
+}
