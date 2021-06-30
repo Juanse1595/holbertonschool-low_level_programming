0750eacd336befd6551a389e7003bd3b87ade207
Juanse1595 <3309@holbertonschool.com>
2021-06-30 02:26:38 +0000

    Fixing 8th program

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
+	
+	if (n > 0)
+	{
+		for (i = 0; i < (n - 1); i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[i]);
+	}
+	else
+		printf("\n");
 }
