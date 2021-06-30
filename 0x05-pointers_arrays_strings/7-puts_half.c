2cdcb6d4bba639c454633d518fcd37f386c53a22
Juanse1595 <3309@holbertonschool.com>
2021-06-30 00:29:45 +0000

    Adding 7th program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of the string
+ * @str: given string
+ * Return
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{}
+	j = (i - 1) / 2;
+	while (j < i)
+	{
+		_putchar(s[j]);
+		j++;
+	}
+}
