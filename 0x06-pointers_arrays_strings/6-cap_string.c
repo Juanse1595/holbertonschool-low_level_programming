e8b3438cc4e32820e6c6aa801f2c1840b1e0dfb2
Juanse1595 <3309@holbertonschool.com>
2021-07-03 02:41:13 +0000

    Fixign 6.. was a symbol

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,9 +8,10 @@ char *cap_string(char *str)
 {
 	int i;
 
-	i = 0;
-	if (str[i] >= 97 && str[i] >= 122)
-		str[i] -= 32;
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] -= 32;
+	}
 	for (i = 1; str[i]; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
