90baee61b69494a5e32b20800c29d8c225423cd0
Juanse1595 <3309@holbertonschool.com>
2021-07-03 02:20:14 +0000

    Fixing 6th program

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	for (i = 1; str[i]; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
-			if (str[i - 1] == ' ' || str[i - 1] == 9 ||
+			if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
 					str[i - 1] == '\n' || str[i - 1]
 					== ',' || str[i - 1] == ';' ||
 					str[i - 1] == '.' || str[i - 1]
@@ -23,7 +23,9 @@ char *cap_string(char *str)
 					== '(' || str[i - 1] == ')' ||
 					str[i - 1] == '{' || str[i - 1]
 					== '}')
+			{
 				str[i] -= 32;
+			}
 	}
 	return (str);
 }
