f9fba26c824a24605a0c4e87bcc2b98f458a087d
Juanse1595 <3309@holbertonschool.com>
2021-07-04 22:47:47 +0000

    Fixing 100th program

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,12 +10,16 @@ char *rot13(char *s)
 	char *arr2 = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 	int i, j;
 
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; arr1[j]; j++)
 		{
 			if (s[i] == arr1[j])
+			{
 				s[i] = arr2[j];
+				j = 52;
+			}
 		}
 	}
 	return (s);
