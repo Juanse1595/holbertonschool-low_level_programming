2d1b9e8b1c6f4f2ebbcd3894a4172074beeca902
Juanse1595 <3309@holbertonschool.com>
2021-07-04 22:20:20 +0000

    Adding 100th program

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+/**
+ * rot13 - using rot13 condes a string
+ * @s: given string
+ * Return: a pointer to the string
+ */
+
+char *rot13(char *s)
+{
+	char *arr1 = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char *arr2 = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; arr1[j]; j++)
+		{
+			if (s[i] == arr1[j])
+			       s[i] = arr2[j];	
+		}
+	}
+	return (s);
+}
