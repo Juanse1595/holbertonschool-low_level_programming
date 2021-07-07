fc7f7ee03fb7cd7cd382d919b123c17cd0ca5dec
Juanse1595 <3309@holbertonschool.com>
2021-07-07 22:01:19 +0000

    Adding 4rd

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion  - returns the value of x to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
