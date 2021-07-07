3b4b7e720b7390c7842d9bb63e594230d49770ae
Juanse1595 <3309@holbertonschool.com>
2021-07-07 21:36:08 +0000

    Adding 3rd

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - returns the factorial number of n
+ * @n: given number
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
