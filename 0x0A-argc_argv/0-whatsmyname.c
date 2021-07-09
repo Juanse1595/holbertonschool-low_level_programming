346c3356ffd63cd13f96e06c3060631c30bcfcf1
Juanse1595 <3309@holbertonschool.com>
2021-07-09 19:22:24 +0000

    Fixing 0 program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: unused
+ * @argv: command line arguments
+ * Return: 0
+ */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
