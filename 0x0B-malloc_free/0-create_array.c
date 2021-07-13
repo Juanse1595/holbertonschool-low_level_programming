9173670e0c719c0a0dd41449b2f588d590652fea
Juanse1595 <3309@holbertonschool.com>
2021-07-13 21:31:58 +0000

    Adding 0th program and holberton.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: specigic char
+ * Return: pointer to the aray
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(sizeof(*p) * size);
+	i = 0;
+	while (i < size)
+	{
+		p[i] = c;
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
