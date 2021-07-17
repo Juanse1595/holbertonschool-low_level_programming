6dedf5ef07b0654f1c8b46290a08fca46285f21d
Juanse1595 <3309@holbertonschool.com>
2021-07-17 03:24:08 +0000

    Adding 100th

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - imitates realloc
+ * @ptr: pointer to the memory
+ * @old_size: old size of the memory
+ * @new_size: new size of the memory
+ * Return: pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+
+	p = NULL;
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size && ptr[i]; i++)
+			p[i] = ptr[i];
+	}
+	else
+	{
+		for (i = 0; i < old_size && ptr[i]; i++)
+			p[i] = ptr[i];
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+	return (p);
+}
