cca45adc460f3070bbc8d22a7fe863121c6e0c12
Juanse1595 <3309@holbertonschool.com>
2021-07-17 01:34:19 +0000

    Fixing 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,13 +11,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i;
 
+	i = 0;
 	p = NULL;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	p = memset(p, 0, nmemb);
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)p + i) = 0;
 	return (p);
 }
