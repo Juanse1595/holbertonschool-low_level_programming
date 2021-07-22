144a68bcf58bd3df804d3b6701a7b03030801b33
Juanse1595 <3309@holbertonschool.com>
2021-07-22 00:43:01 +0000

    Adding all 4 files of the 3rd program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calls function and prints result
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 if success, otherwise if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' && *argv[3] == '0') || (*argv[2] == '%' && *argv[2] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	p = get_op_func(argv[2]);
+	printf("%d\n", p(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - adds 2 ints
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract 2 ints
+ * @a: first int
+ * @b: second int
+ * Return: substract
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies 2 ints
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides 2 ints
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of a division
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
