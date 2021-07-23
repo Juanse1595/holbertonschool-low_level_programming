94686d5cdeba8e13af954e9a521d1776e4ad95e3
Juanse1595 <3309@holbertonschool.com>
2021-07-23 23:13:05 +0000

    Adding 1rst program

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints numbers passed as asrguments,
+ * separated by a separator
+ * @separator: separator
+ * @n: number of numbers to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list argp;
+
+	if (n == 0)
+		return;
+	va_start(argp, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			printf("%d", va_arg(argp, int));
+		else if (i < (n - 1))
+			printf("%d%s", va_arg(argp, int), separator);
+		else
+			printf("%d", va_arg(argp, int));
+	}
+	printf("\n");
+}
