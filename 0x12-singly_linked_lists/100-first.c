6cec44b8cada7ec5ea83aae50b5ee5ef89a945dd
Juanse1595 <3309@holbertonschool.com>
2021-07-31 05:02:10 +0000

    Adding 100th

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * print - print before main
+  */
+
+void print(void) __attribute__ ((constructor));
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
