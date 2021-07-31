c03ffaffbe0bbd6631897e97b37e04c5b787cc81
Juanse1595 <3309@holbertonschool.com>
2021-07-31 01:04:15 +0000

    Updating 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,17 @@
+/**
+  * print_list - Prints all elements in list
+  * @h: head pointer
+  * Return: number of nodes
+  */
+
 size_t print_list(const list_t *h)
 {
+	size_t nodecount = 0;
+	char *s = "(nil)";
 
+	while (h != NULL)
+	{
+
+	}
+	return (0);
 }
