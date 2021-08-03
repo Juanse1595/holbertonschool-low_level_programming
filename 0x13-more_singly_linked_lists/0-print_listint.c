47fe97225113adbd8d4c4c3838410a3c872755b1
Juanse1595 <3309@holbertonschool.com>
2021-08-03 15:38:04 +0000

    Fixing betty on header and 0th

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
   * print_listint - Prints all the elements of a listint_t
-  list
+  * list
   * @h: pointer to the list
   * Return: Number of nodes
   */
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count ++;
+		count++;
 	}
 	return (count);
 }
