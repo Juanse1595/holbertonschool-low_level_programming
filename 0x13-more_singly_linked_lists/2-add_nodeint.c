8c1c191b83f4f77073a7aca59bfab5124a9eab58
Juanse1595 <3309@holbertonschool.com>
2021-08-03 16:16:27 +0000

    Updating header and 2nd program

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,9 +9,11 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint *p = NULL;
+	listint_t *p = NULL;
 
 	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+		return (NULL);
 	p->n = n;
 	p->next = *head;
 	*head = p;
