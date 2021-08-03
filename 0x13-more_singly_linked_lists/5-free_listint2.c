74cd549f40c44bdc79df71e2c61c28a59dc583bb
Juanse1595 <3309@holbertonschool.com>
2021-08-03 22:00:06 +0000

    Adding 5th program

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a list of nodes
+  * @head: pointer to head
+  */
+
+void free_listint2(listint_t **head)
+{
+	/* base case*/
+	if (*head == NULL)
+		return;
+	/* recursive case */
+	free_listint2(&(*head)->next);
+	free(*head);
+}
