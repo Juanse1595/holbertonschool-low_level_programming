f2ffadad95765b20801fec738a69a2138882c909
Juanse1595 <3309@holbertonschool.com>
2021-08-04 05:01:37 +0000

    Adding 100th

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverses a linked list
+  * @head: pointer to head
+  * Return pointer to first node
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p, *p2;
+
+	if (head == NULL)
+		return (NULL);
+	p2 = NULL;
+	while (*head != NULL)
+	{
+		p = (*head)->next;
+		(*head)->next = p2;
+		p2 = *head;
+		*head = p;
+	}
+	*head = p2;
+	return (*head);
+}
