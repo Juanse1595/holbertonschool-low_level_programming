7f306fae3261f1db7d6706b2c7ba350103672f44
Juanse1595 <3309@holbertonschool.com>
2021-08-10 04:54:38 +0000

    Fixing 3rd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+  * error_check - checks for errors
+  * @status: the exit status
+  * @file: name of file
+  * @fd: file descriptor
+  */
+
+void error_check(int status, char *file, int fd)
+{
+	switch (status)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(97);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+			exit(98);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+			exit(99);
+		case 100:
+			dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
+			exit(100);
+
+	}
+}
+
+
+/**
   * main - copies the content of a file to another
   * @argc: number of arguments
   * @argv: arguments
@@ -9,39 +37,31 @@
 
 int main(int argc, char *argv[])
 {
-	int fd, fd2, rd_ret, wr_ret, cl1, cl2;
-	char *buffer = malloc(1024);
+	int fd, fd2, rd_ret = 1, wr_ret, cl1, cl2;
+	char *buffer[1024];
 
 	if (argc != 3)
+		error_check(97, NULL, 0);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		error_check(98, argv[1], 0);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 < 0)
+		error_check(99, argv[2], 0);
+	while (rd_ret > 0)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd = open(argv[1], O_RDWR);
-	rd_ret = read(fd, buffer, 1024);
-	if (fd < 0 || rd_ret < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	wr_ret = write(fd2, buffer, rd_ret);
-	if (fd2 < 0 || wr_ret < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		rd_ret = read(fd, buffer, 1024);
+		if (rd_ret < 0)
+			error_check(98, argv[1], 0);
+		wr_ret = write(fd2, buffer, rd_ret);
+		if (wr_ret < 0)
+			error_check(99, argv[2], 0);
 	}
 	cl1 = close(fd);
 	cl2 = close(fd2);
 	if (cl1 < 0)
-	{
-		dprintf(2, "Can't close fd %d\n", fd);
-		exit(100);
-	}
+		error_check(100, NULL, fd);
 	if (cl2 < 0)
-	{
-		dprintf(2, "Can't close fd %d\n", fd2);
-		exit(100);
-	}
+		error_check(100, NULL, fd2);
 	return (0);
 }
