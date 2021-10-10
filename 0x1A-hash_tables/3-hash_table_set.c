d976922f109a9b6c755bf7f715570d90ed7f6868
Juanse1595 <3309@holbertonschool.com>
2021-10-10 21:05:35 +0000

    FIX 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,23 +12,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *cp_value = NULL, *cp_key = NULL;
 	unsigned long int index = 0;
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *node_pointer = NULL;
 
 	cp_key = strdup(key);
-	if (cp_key == NULL || *cp_key == '\0')
+	if (key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)cp_key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
 	cp_value = malloc(strlen(value) + 1);
 	strcpy(cp_value, value);
+	/* in case the key already exists */
+	node_pointer = ht->array[index];
+	while (node_pointer != NULL)
+	{
+		if (strcmp(node_pointer->key, cp_key) == 0)
+		{
+			node_pointer->value = cp_value;
+			return (1);
+		}
+		node_pointer = node_pointer->next;
+	}
+
+	/* in case the key is unique */
+	new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
 	new_node->key = cp_key;
 	new_node->value = cp_value;
-	if (ht->array[index] != NULL) /*in case of collision*/
-		new_node->next = ht->array[index];
-	else
-		new_node->next = NULL;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
