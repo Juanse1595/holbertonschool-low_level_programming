3b01bea1a76c08cb20087b0beffdb497f9694d1a
Juanse1595 <3309@holbertonschool.com>
2021-10-11 03:10:57 +0000

    fix 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node_pointer = NULL;
 
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	node_pointer = ht->array[index];
 	while (node_pointer != NULL)
