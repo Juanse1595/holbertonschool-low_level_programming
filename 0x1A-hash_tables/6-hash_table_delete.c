bd3b14a62e1c1a1c25344cfacc2ad6a0bce48fa1
Juanse1595 <3309@holbertonschool.com>
2021-10-11 01:28:39 +0000

    ADD 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_delete - deletes hash table
+  * @ht: hash table to delete
+  */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current = NULL, *prev = NULL;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		current = ht->array[index];
+		prev = current;
+		while (current != NULL)
+		{
+			current = current->next;
+			free(prev);
+			prev = current;
+		}
+	}
+	free(ht->array);
+	free(ht);
+	ht = NULL;
+	current = NULL;
+	prev = NULL;
+}
